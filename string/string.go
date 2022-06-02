a765027e1b92b780ad8f4b54103f67e40ef46dcb
so1ve <nn_201312@163.com>
2022-06-02 19:42:04 +0800

    feat: optional end param

--- string/string.go
+++ string/string.go
@@ -1,6 +1,10 @@
 package su_string
 
-func Substring(s string, start, end int) string {
+func Substring(s string, start int, args ...int) string {
+	if len(args) > 1 {
+		panic("Only one extra parameter should be passed")
+	}
 	runes := []rune(s)
+	end := args[0]
 	return string(runes[start:end])
 }
